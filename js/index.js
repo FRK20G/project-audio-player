5333d614b818355b229b8debcde9951de3e87a4f
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2020-11-23 09:49:29 +0100

    Removed stuff

--- js/index.js
+++ js/index.js
@@ -38,32 +38,3 @@ audio.addEventListener('timeupdate', () => {
     const percent = (audio.currentTime / audio.duration) * 100;
     progressBar.style.width = `${percent}%`;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//https://p.scdn.co/mp3-preview/a3b5cf9da8473c959c6833e75404379db9226ba7?cid=774b29d4f13844c495f206cafdad9c86
-//When Christmas Comes to Town
-//Matthew Hall, Meagan Moore
-
-//https://p.scdn.co/mp3-preview/ad04264bcbf286030f90895dacdc2af00e586c99?cid=774b29d4f13844c495f206cafdad9c86
-//Spirit of the season
-//Alan Silvestri
-
-//https://p.scdn.co/mp3-preview/729371ac317464304d4ca3511653bbe866ac7cef?cid=774b29d4f13844c495f206cafdad9c86
-//Suite from The Polar Express
-//Alan Silvestri
